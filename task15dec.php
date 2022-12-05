051bffab26128c6eecdb7352e26b533dd619b9b1
SIVA\sivak <sivakolli7482@gmail.com>
2022-12-05 16:11:07 +0530

    php file added

---
+++ task15dec.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>5th dec</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" >
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <div class="container">
+     <div class="row">
+      <div class="col-6 offset-3 bg-light mt-3">
+        <div>
+           <i class="fa fa-brands fa-whatsapp"></i>
+           <p class="text-primary">Login</p>
+        </div>        
+        <div class="mb-3">
+                  <label for="emailAddress" class="form-label">Email addresss</label>
+                  <input type="text" class="form-control" id="emailAddress" placeholder=".......@gmail.com">
+        </div>
+        <div class="mb-3">
+                  <label for="passWord" class="form-label">Password</label>
+                  <input type="text" class="form-control" id="passWord" placeholder="**********">
+        </div>
+            <input type="checkbox"/>
+            <label>Check me out</label>
+            <button class="btn btn-primary">Submit</button>
+      </div>
+     </div>
+    </div>
+</body>
+</html>
